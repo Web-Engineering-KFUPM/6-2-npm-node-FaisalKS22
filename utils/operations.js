47eeee8c94be8c9704289aea5ea8a76c6919b07d
Baashn <s202261280@ccm.kfupm.edu.sa>
2026-04-01 11:49:42 +0300

    week6.2

---
+++ utils/operations.js
@@ -0,0 +1,19 @@
+export function add(numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
+
+export function subtract(numbers) {
+  return numbers.reduce((result, num, index) => index === 0 ? num : result - num);
+}
+
+export function multiply(numbers) {
+  return numbers.reduce((product, num) => product * num, 1);
+}
+
+export function divide(numbers) {
+  return numbers.reduce((result, num, index) => {
+    if (index === 0) return num;
+    if (num === 0) return NaN;
+    return result / num;
+  });
+}
